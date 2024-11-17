63e236f4c161a0d764b8a2e2e9169ae4824a974c
YuAnKwon <158270520+YuAnKwon@users.noreply.github.com>
2024-11-17 21:51:58 +0900

    Update A＋B － 3.java

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Scanner 객체를 이용해 입력을 받음.
         Scanner sc = new Scanner(System.in);
 
-        int T, A, B;
+        // 테스트 케이스의 개수 T를 입력받음.
+        int T = sc.nextInt();
 
-        T = sc.nextInt();
-
+        // T번만큼 반복하여 각 테스트 케이스를 처리
         for (int i=0; i<T; i++){
-            A = sc.nextInt();
-            B = sc.nextInt();
+            // 각 테스트 케이스에서 A와 B를 입력받음.
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            // A+B를 출력.
             System.out.println(A+B);
         }
 
+        // Scanner 객체를 닫음.
         sc.close();
     }
 }
