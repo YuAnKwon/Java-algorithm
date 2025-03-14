fcf5f62ffc56b0352350b6686d2218ca9635a14b
YuAnKwon <158270520+YuAnKwon@users.noreply.github.com>
2025-03-14 14:13:25 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -5,26 +5,21 @@ public class Main{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int B = 42;
-        int[] array = new int[10];
         int count = 0;
+        boolean[] array = new boolean[42];
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < 10; i++) {
             int A = Integer.parseInt(bf.readLine());
             int remainder = A % B;
+            array[remainder] = true;
+        }
 
-            boolean isDuplicate = false;
-            for (int j = 0; j < count; j++) {
-                if(array[j] == remainder){
-                    isDuplicate = true;
-                    break;
-                }
-            }
-
-            if(!isDuplicate){
-                array[count] = remainder;
+        for (boolean value : array) {
+            if(value){
                 count++;
             }
         }
+
         System.out.println(count);
     }
 }
