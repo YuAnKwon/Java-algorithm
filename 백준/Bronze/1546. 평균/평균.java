950699a451f33ba603e82dbdb466cf36544d9326
YuAnKwon <158270520+YuAnKwon@users.noreply.github.com>
2025-04-16 15:37:50 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); // 시험 본 과목 개수 N
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 현재 성적
+
+        // 점수 배열
+        int[] arr = new int[N];
+        int M = 0; // 점수 중 최댓값
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); // 배열에 점수 추가
+            if (M < arr[i]) M = arr[i]; // 최댓값 구하기
+        }
+
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += (double) arr[i] / M * 100;
+        }
+
+        double average = sum / N;
+
+        bw.write(average+"\n");
+
+        bw.close();
+        br.close();
+    }
+}
