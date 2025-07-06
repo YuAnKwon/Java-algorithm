4f7130585944fb5c6a35a7ab007c560b41ea14be
YuAnKwon <158270520+YuAnKwon@users.noreply.github.com>
2025-07-06 12:08:17 +0900

    Update 약수의 개수와 덧셈.java

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -3,12 +3,12 @@ class Solution {
 		int answer = 0;
 
 		for (int i = left; i <= right; i++) {
-			int count = 0;
+			int cnt = 0;
 			for (int j = 1; j <= i; j++) {
 				if (i % j == 0)
-					count++;
+					cnt++;
 			}
-			answer = count % 2 == 0 ? answer + i : answer - i;
+			answer = cnt % 2 == 0 ? answer + i : answer - i;
 		}
 		return answer;
 	}
